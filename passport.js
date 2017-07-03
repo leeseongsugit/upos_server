7cfb0a4fe3ac2a7115e27f5573f1048995f94925
2010054815 <2010054815@hanyang.ac.kr>
2017-07-03 15:50:18 +0900

    init file + login

---
+++ passport.js
@@ -0,0 +1,32 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-session').Strategy;
+var Users = require('./user');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  passport.deserializeUser((user, done) => {
+    User.findById(id, (err, user) => {
+      done(null, user);
+    });
+  });
+
+  passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'password',
+    session: true,
+    passReqToCallback: false,
+  }, (id, password, done) => {
+    users.findOne({id: id}, (findError, user) => {
+      if(findError) return done(findError);
+      if(!user) return done(null, false, { message: 'not present id'});
+      return user.comparePassword(password, (passError, isMatch) => {
+        if(isMatch) {
+          return done(null, user);
+        }
+        return done(null, false, { message: 'not equal password'});
+      });
+    });
+  }));
+};
