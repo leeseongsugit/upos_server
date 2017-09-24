63826a3f09fc2e75c9555d4d36c517635a41dee4
leeseongsugit <harunokaje33@gmail.com>
2017-09-25 02:42:54 +0900

    adjust code comment

--- routes/filemanage.js
+++ routes/filemanage.js
@@ -28,18 +28,18 @@ router.get('/', function(req,res){
       //res.json(data);
     });
 });
-
+//GET CONTENTS BY content_id not activate
 router.get('/api/:content_id', function(req,res){
   Content.find({_id: req.params.content_id}, function(err, data){
     if(err)
       return res.status(500).send({error: err});
-    if(!book)
+    if(!data)
       return res.status(404).send({error: 'book not found'});
     res.render('./filemanage/filepresent', {contents : data});
   });
 });
 
-// GET CONTENTS BY WRITER
+// GET CONTENTS BY WRITER not activate
 router.get('/api/:writer', function(req, res){
     Content.find({writer: req.params.writer},  function(err, data){
         if(err)
@@ -95,7 +95,6 @@ router.get('/show/:content_id', function(req,res){
 router.get('/present/:content', function(req, res){
   //pdf.js와 연동
 })
-//진행중
 router.get('/download/:content_id', function(req, res){
   Content.findOne({_id : req.params.content_id}, function(err, data){
     var path = data.upFile[0].path;
@@ -105,64 +104,7 @@ router.get('/download/:content_id', function(req, res){
     res.download(path, fileName);
   });
 });
-/*//업로드 - 분산처리
-router.post('/upload', function(req, res){
-  var title = req.body.title;  //inputText의 name Value를 가져옴
-  var writer = req.body.writer;
-  var contents = req.body.contents;
-  var weeks = req.body.weeks;
-  var type = req.body.type;
-  var fileObj = req.files.userfile;
-  var orgFileName = fileObj.originalname;  //원본파일명 저장
-  var savePath = __dirname + '/../public/upload/' + saveFileName;  //저장된 파일명
-  //get gfs output stream
-  var writeStream = gfs.createWriteStream({
-    'filename': orgFileName,
-    'metadata': {
-      'title': title
-      'writer': writer
-      'contents': contents
-      'weeks': weeks
-      'type': type
-    }
-  });
-  //경로에 업데이트 된 파일을 읽어들여서 girdfs 출력 스트림에 연결
-  fs.createReadStream(savePath).pipe(writeStream);
-  //스트림 연결 종료 -> close 이벤트 발생
-  writeStream.on('close', function(file){
-    //몽고DB는 데이터 저장시 무조건 파일시스템을 거쳐가기 때문에 파일시스템의 데이터는 삭제
-    fs.unlink(savePath, function() {});
-    res.send('ok');
-  });
-});
 
-
-//Download 요청 처리
-router.get('/download/:id', function(req, res){
-  var _id = req.query.id;
-  gfs.files.find({'_id': objecDate.nowtId})
-    .toArray(function(err, files) {
-      if(err) res.send(err);
-      var filename = files[0].filename;
-      //gfs에서 읽어올 스트림 객체 가져오기
-      var readStream = gfs.createReadStream({'_id': _id});
-      //Download
-      res.semyFiletHeader("Content-Disposition", "attachment;filename=" + encodeURI(filename));
-      res.setHeader("Content-Type","application/octet-stream");
-      //readStream에 있는 Data를 res객체에 옮긴다
-      readStream.pipe(res);
-  })
-});
-
-//삭제 요청 처리
-router.get('/delete/:id', function(req, res){
-  var _id = req.query.id;
-  gfs.remove({'_id': ObjectID(_id)}, function(err) {
-    if(err) res.send(err);
-    res.end("ok");
-  });
-});
-*/
 function day_time(){
   var now = new Date();
   year = "" + now.getFullYear();
